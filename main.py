89eee932339027d591c62a41fcbc8783b76992d6
Luis Domingues <luisgsilva950@gmail.com>
2022-05-26 23:26:44 -0300

    fix file

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
+import collections
 import copy
 import random
 import time
 
 import numpy
 from typing import List
+import json
+
+
+def column(matrix, i):
+    return [row[i] for row in matrix]
 
 
 class Coordinate:
@@ -29,8 +35,10 @@ class Customer:
 
 
 class F2ProblemDefinition:
-    min_customers_success_tax = 0.95
+    min_customers_attended = 570
     max_customer_to_point_distance = 85
+    max_active_points = 25
+    max_consumed_capacity = 150
 
     def __init__(self, customers: List[Customer], points: List[Point]):
         self.customers = customers
@@ -40,6 +48,7 @@ class F2ProblemDefinition:
         self.penal_fitness = 0.0
         self.penal = 0
         self.customer_point_distances = []
+        self.points_status = [False for _ in range(10201)]
         self.solution = []
 
     @staticmethod
@@ -67,21 +76,43 @@ class F2ProblemDefinition:
 
     def objective_function(self) -> 'F2ProblemDefinition':
         total_distance = 0
-        for index, custom_active_points in enumerate(self.solution):
-            for active_index, active in enumerate(custom_active_points):
+        total_active_points = sum(self.points_status)
+        customers_attended_count = self.get_customers_attended_count()
+        consumed_capacity_per_point = self.get_consumed_capacity()
+        for customer_index, customer_active_points in enumerate(self.solution):
+            for point_index, active in enumerate(customer_active_points):
                 if active:
-                    distance = self.customer_point_distances[index][active_index]
+                    consumed_capacity = consumed_capacity_per_point[point_index]
+                    distance = self.customer_point_distances[customer_index][point_index]
                     total_distance = total_distance + distance
-                    # capacity_consumed = 0
-                    # for i in range(0, 600):
-                    #     if self.solution[i][active_index]:
-                    #         capacity_consumed = capacity_consumed + self.customers[i].consume
-                    self.penal = self.penal + (
-                        (
-                                distance - self.max_customer_to_point_distance) if distance > self.max_customer_to_point_distance else 0) ** 3
+                    if distance > self.max_customer_to_point_distance:
+                        self.penal = self.penal + (distance - self.max_customer_to_point_distance) ** 3
+                    if consumed_capacity > self.max_consumed_capacity:
+                        self.penal = self.penal + (consumed_capacity - self.max_consumed_capacity) ** 3
+        if total_active_points > self.max_active_points:
+            self.penal = self.penal + (total_active_points - self.max_active_points) ** 3
+        print(f"Customers attended: {customers_attended_count} - Total active points: {total_active_points}")
+        if customers_attended_count < self.min_customers_attended:
+            self.penal = self.penal + (self.min_customers_attended - customers_attended_count) ** 3
         self.fitness = total_distance
         self.penal_fitness = self.fitness + self.penal
         return self
+
+    def get_customers_attended_count(self) -> int:
+        customers_attended_count = 0
+        for customer_points in self.solution:
+            customers_attended_count = customers_attended_count + max(customer_points)
+        return customers_attended_count
+
+    def get_consumed_capacity(self) -> dict:
+        consumed_capacity_per_point = collections.defaultdict(float)
+        for point_index, is_used_point in enumerate(self.points_status):
+            if is_used_point:
+                point_customers = column(self.solution, point_index)
+                for customer_index, is_point_active_in_customer in enumerate(point_customers):
+                    if is_point_active_in_customer:
+                        consumed_capacity_per_point[point_index] += self.customers[customer_index].consume
+        return consumed_capacity_per_point
 
     def neighborhood_change(self, y: 'F2ProblemDefinition'):
         if y.penal_fitness < self.penal_fitness:
@@ -102,19 +133,34 @@ class F2ProblemDefinition:
         elif self.k == 2:
             for _ in range(5):
                 customer = random.randint(0, 600)
-                y.solution[customer] = [0 for _ in range(10201)]
+                y.solution[customer] = [False for _ in range(10201)]
         elif self.k == 3:
             for _ in range(5):
                 customer = random.randint(0, 600)
                 index_max = numpy.argmax(y.solution[customer])
                 y.solution[customer] = [p == min([index_max + 2, 10201]) for p in range(10201)]
+        for i in range(10201):
+            self.points_status[i] = max(column(self.solution, i))
         return y
+
+    def save_points_distances_on_file(self):
+        with open('customer_point_distances.json', 'w') as f:
+            json.dump(self.customer_point_distances, f)
+
+    def get_points_distances_from_file(self):
+        with open('customer_point_distances.json', 'r') as f:
+            value = json.load(f)
+        return value or []
 
     def get_initial_solution(self) -> 'F2ProblemDefinition':
         for customer_index, customer in enumerate(self.customers):
             customer_bool_solutions = []
-            distances = [customer.point.get_distance(p) for p in self.points]
-            self.customer_point_distances.append(distances)
+            self.customer_point_distances = self.get_points_distances_from_file()
+            if self.customer_point_distances:
+                distances = self.customer_point_distances[customer_index]
+            else:
+                distances = [customer.point.get_distance(p) for p in self.points]
+                self.customer_point_distances.append(distances)
             index_min = numpy.argmin(distances)
             for index, point in enumerate(self.points):
                 if index == index_min:
@@ -122,6 +168,10 @@ class F2ProblemDefinition:
                 else:
                     customer_bool_solutions.append(False)
             self.solution.append(customer_bool_solutions)
+        self.save_points_distances_on_file()
+        for i in range(10201):
+            self.points_status[i] = max(column(self.solution, i))
+        print(f"Total active points on initial solution: {sum(self.points_status)}")
         return self
 
 
